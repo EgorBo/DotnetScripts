7b271b68bd1548659bf5e68c68ff36292b3f32a2
egorbot <egorbot@build.foo>
2024-11-14 22:44:42 +0100

    initial commit

---
+++ dotnet-installer.sh
@@ -0,0 +1,16 @@
+# A wrapper over dotnet-install.sh script
+
+function installDotnet () {
+    DOTNET_VERSION="${1:-8.0}"
+    export DOTNET_ROOT="${2:-$(pwd)/dotnet}"
+    if [ ! -f "${DOTNET_ROOT}/dotnet-install.sh" ]; then
+        mkdir -p ${DOTNET_ROOT}
+        echo "Downloading dotnet-install.sh script into ${DOTNET_ROOT}"
+        wget https://dot.net/v1/dotnet-install.sh -O ${DOTNET_ROOT}/dotnet-install.sh
+        chmod +x ${DOTNET_ROOT}/dotnet-install.sh
+    fi
+    ${DOTNET_ROOT}/./dotnet-install.sh -Channel $DOTNET_VERSION -InstallDir ${DOTNET_ROOT}
+    if [ "$(which dotnet)" != "${DOTNET_ROOT}/dotnet" ]; then
+        export PATH=${DOTNET_ROOT}:${DOTNET_ROOT}/tools:${PATH}
+    fi
+}
