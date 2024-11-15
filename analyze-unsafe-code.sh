035415ac1c1d37d734461f844ca1259137889c7a
Egor Bogatov <egorbo@gmail.com>
2024-11-15 01:51:40 +0100

    Update analyze-unsafe-code.sh

--- analyze-unsafe-code.sh
+++ analyze-unsafe-code.sh
@@ -3,7 +3,8 @@
 # Install local dotnet
 wget -O dotnet-installer.sh \
 https://raw.githubusercontent.com/EgorBo/DotnetScripts/refs/heads/main/dotnet-installer.sh && \
-chmod +x dotnet-installer.sh && source dotnet-installer.sh
+chmod +x dotnet-installer.sh
+. dotnet-installer.sh
 installDotnet 8.0
 installDotnet 9.0
 #
